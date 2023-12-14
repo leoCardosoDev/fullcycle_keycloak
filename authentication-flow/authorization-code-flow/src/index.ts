1cd35f7ff975cd13aa402ae33c17d9ec8dc3d913
leoCardosoDev <leocardosodev@gmail.com>
2023-12-14 14:49:35 -0300

    feat: add redirect to keycloack

--- authentication-flow/authorization-code-flow/src/index.ts
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -2,8 +2,16 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/login', (req, res) => {
+  const loginParams = new URLSearchParams({
+    client_id: 'fullcycle-client',
+    redirect_uri: 'http://localhost/3000/callback',
+    response_type: 'code',
+    scope: 'openid',
+    })
+  const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`
+  console.log(url)
+  res.redirect(url)
 })
 
 app.listen(3000, () => {
